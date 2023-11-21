679cf1e046c59e7348ce1f4bec4e3a4bfe61088c
jiangyanpeng <jiangyanpeng@sensetime.com>
2023-11-21 10:55:04 +0800

    :recycle: 增加help函数

--- tests/test_base.cc
+++ tests/test_base.cc
@@ -1,13 +1,13 @@
 #include <fstream>
 
-#include "test_util.h"
+#include "utils/test_util.h"
 #include "gtest/gtest.h"
 
-class ImwriteTest : public ::testing::Test {
+class ImageTest : public ::testing::Test {
 protected:
     void SetUp() override {}
 };
 
-TEST_F(ImwriteTest, PackageBGRU8_PositiveInput) {}
+TEST_F(ImageTest, InvalidInput) {}
 
-TEST(ImwriteTest, PositiveInput) {}
+TEST(ImageTest, ValidInput) {}
